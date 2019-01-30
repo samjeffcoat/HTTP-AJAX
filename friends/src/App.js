afc88d9a8432666573bacaaad96781687c895368
samjeffcoat <samjeffcoat@gmail.com>
2019-01-30 14:30:48 -0600

    Sets state to friends array and displays headline of Friends List

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-class App extends Component {
+import "./App.css";
+
+class App extends React.Component {
+  /*Sets state to friends array*/
+  state = {
+    friends: []
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Friends list</h1>
       </div>
     );
   }
